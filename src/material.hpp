2008086a69976a8ae650d420bbb692453e8a3cc6
SeungWoo Yoo <dreamy1534@gmail.com>
2020-12-28 17:59:30 +0900

    feat(develop): Add comments to function 'scatter' of class 'metal'

--- src/material.hpp
+++ src/material.hpp
@@ -38,10 +38,16 @@ public:
             const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
             ) const override
     {
+        // determine the direction of reflected ray
         vector3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
 
-        scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
+        /**
+         * generate a ray object that originates from the point of incidence, and spreads out toward certain direction
+         * the direction might be randomized according to the fuzziness of this material
+         */
+        scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
         attenuation = albedo;
+        // if fuzziness is too high, the light may not be reflected off the surface (rather, it seems to be absorbed)
         return (dot_product(scattered.direction(), rec.normal) > 0);
     }
 
