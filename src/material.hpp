59f5e282851355bbed58d917bb9443b1ef7a2f1b
Yoo SeungWoo <dreamy1534@gmail.com>
2020-12-28 21:06:22 +0900

    feat(develop): Add comment to 'material.hpp'

--- src/material.hpp
+++ src/material.hpp
@@ -71,7 +71,7 @@ public:
             const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
             ) const override {
         attenuation = color(1.0, 1.0, 1.0);
-        double refraction_ratio = rec.front_face ? (1.0/ir) : ir;
+        double refraction_ratio = rec.front_face ? (1.0/ir) : ir;   // if incident ray is from the inside of dielectric, (ratio of IOR) = (index of dielectric) / 1.0 (air)
 
         vector3 unit_direction = unit_vector(r_in.direction());
         double cos_theta = fmin(dot_product(-unit_direction, rec.normal), 1.0);
