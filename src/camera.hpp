d998ce4f41f7555c863d9aeb20277bf41ecb231c
Yoo SeungWoo <dreamy1534@gmail.com>
2020-12-28 21:55:32 +0900

    feat(develop): Add adjustable VFOV to camera class
    Given the vertical field-of-view in degrees and aspect ratio, camera class is automatically initializes its viewport plane

--- src/camera.hpp
+++ src/camera.hpp
@@ -10,12 +10,21 @@
 class camera
 {
 public:
-    camera()
-    {
-         lower_left_corner = point3(-2.0, -1.0, -1.0);      // global coordinate of upper left corner of the viewport
-         horizontal = vector3(4.0, 0.0, 0.0);   // horizontal basis within the viewport plane
-         vertical = vector3(0.0, 2.0, 0.0);     // vertical basis within the viewport plane
-         origin = point3(0.0, 0.0, 0.0);    // global coordinate of camera itself
+    camera(
+            double vfov, // vertical field-of-view in degrees
+            double aspect_ratio
+            ) {
+            auto theta = degress_to_radians(vfov);
+            auto h = tan(theta/2);
+            auto viewport_height = 2.0 * h;
+            auto viewport_width = aspect_ratio * viewport_height;
+
+            auto focal_length = 1.0;
+
+            origin = point3(0, 0, 0);
+            horizontal = vector3(viewport_width, 0.0, 0.0);
+            vertical = vector3(0.0, viewport_height, 0.0);
+            lower_left_corner = origin - horizontal/2 - vertical/2 - vector3(0, 0, focal_length);
     }
 
     ray get_ray(double u, double v) const
