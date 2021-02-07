542b605033fd146d82a76773b9d1d697f6a79ae0
SeungWoo Yoo <dreamy1534@gmail.com>
2021-02-07 12:49:31 +0900

    feat(cuda): Implement operations required
    - reflection, refraction in vector operations
    - operation which set the 'front_face' variable

--- src/gpu/sphere.hpp
+++ src/gpu/sphere.hpp
@@ -30,6 +30,7 @@ __device__ bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& 
             rec.t = temp;
             rec.p = r.at(rec.t);
             rec.normal = (rec.p - center) / radius;
+            rec.set_face_normal(r, rec.normal);
             rec.mat_ptr = mat_ptr;
             return true;
         }
@@ -39,6 +40,7 @@ __device__ bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& 
             rec.t = temp;
             rec.p = r.at(rec.t);
             rec.normal = (rec.p - center) / radius;
+            rec.set_face_normal(r, rec.normal);
             rec.mat_ptr = mat_ptr;
             return true;
         }
--- src/gpu/vector3.hpp
+++ src/gpu/vector3.hpp
@@ -8,7 +8,11 @@
 class vector3  {
 
 public:
-    __host__ __device__ vector3() {}
+    __host__ __device__ vector3() : e{0, 0, 0}
+    {
+        // Do nothing
+    }
+
     __host__ __device__ vector3(float e0, float e1, float e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
     __host__ __device__ inline float x() const { return e[0]; }
     __host__ __device__ inline float y() const { return e[1]; }
@@ -29,8 +33,8 @@ public:
     __host__ __device__ inline vector3& operator*=(const float t);
     __host__ __device__ inline vector3& operator/=(const float t);
 
-    __host__ __device__ inline float length() const { return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]); }
-    __host__ __device__ inline float squared_length() const { return e[0]*e[0] + e[1]*e[1] + e[2]*e[2]; }
+    __host__ __device__ inline double length() const { return (double)sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]); }
+    __host__ __device__ inline double squared_length() const { return e[0]*e[0] + e[1]*e[1] + e[2]*e[2]; }
     __host__ __device__ inline void make_unit_vector();
 
 
@@ -83,8 +87,8 @@ __host__ __device__ inline vector3 operator*(const vector3 &v, float t) {
     return vector3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
-__host__ __device__ inline float dot(const vector3 &v1, const vector3 &v2) {
-    return v1.e[0] *v2.e[0] + v1.e[1] *v2.e[1]  + v1.e[2] *v2.e[2];
+__host__ __device__ inline double dot(const vector3 &v1, const vector3 &v2) {
+    return v1.e[0] * v2.e[0] + v1.e[1] *v2.e[1] + v1.e[2] *v2.e[2];
 }
 
 __host__ __device__ inline vector3 cross(const vector3 &v1, const vector3 &v2) {
@@ -140,6 +144,28 @@ __host__ __device__ inline vector3& vector3::operator/=(const float t) {
 
 __host__ __device__ inline vector3 unit_vector(vector3 v) {
     return v / v.length();
+}
+
+#define RANDVEC3 vector3(curand_uniform(local_rand_state),curand_uniform(local_rand_state),curand_uniform(local_rand_state))
+
+__device__ vector3 random_in_unit_sphere(curandState *local_rand_state) {
+    vector3 p;
+    do {
+        p = 2.0f * RANDVEC3 - vector3(1, 1, 1);
+    } while (p.squared_length() >= 1.0f);
+    return p;
+}
+
+__device__ vector3 reflect(const vector3& v, const vector3& n) {
+    return v - 2.0f * dot(v, n) * n;
+}
+
+__device__ vector3 refract(const vector3& v, const vector3& n, double etai_over_etat) {
+    auto cos_theta = fmin(dot(-v, n), 1.0);
+    vector3 refracted_perp = etai_over_etat * (v + cos_theta * n);
+    vector3 refracted_parallel = -sqrt(fabs(1.0 - refracted_perp.squared_length())) * n;
+    vector3 refracted = refracted_perp + refracted_parallel;
+    return refracted;
 }
 
 #endif
