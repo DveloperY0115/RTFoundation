7fb912845cdc4b7049a5a0cf500dec2d93817b04
SeungWoo Yoo <dreamy1534@gmail.com>
2021-02-14 18:04:41 +0900

    feat(cuda): Add variable 'time' to ray

--- src/gpu/ray.hpp
+++ src/gpu/ray.hpp
@@ -12,34 +12,34 @@
 
 class ray{
 public:
-    __device__ ray()
-    {
+    __device__ ray() {
         // Do nothing
     }
     __device__ ray(const vector3& origin, const vector3& direction)
-            : orig(origin), dir(direction)
-    {
+            : orig(origin), dir(direction) {
         // Do nothing
     }
 
-    __device__ vector3 origin() const
-    {
+    __device__ vector3 origin() const {
         return orig;
     }
 
-    __device__ vector3 direction() const
-    {
+    __device__ vector3 direction() const {
         return dir;
     }
 
-    __device__ vector3 at(float t) const
-    {
+    __device__ vector3 at(float t) const {
         return orig + t * dir;
+    }
+
+    __device__ double time() const {
+        return tm;
     }
 
 public:
     vector3 orig;
     vector3 dir;
+    double tm;
 };
 
 #endif //FIRSTRAYTRACER_RAY_HPP
