5540fb9152c04352bc9e2e8741b04343b37717b5
Yoo SeungWoo <dreamy1534@gmail.com>
2021-12-19 17:44:07 +0900

    Implement 'Hit' method of 'MovingSphere'

--- rt-the-next-week/src/cpu/Geometry/MovingSphere.hpp
+++ rt-the-next-week/src/cpu/Geometry/MovingSphere.hpp
@@ -17,11 +17,11 @@ public:
                  double StartMovingAt,
                  double StopMovingAt,
                  double Radius,
-                 shared_ptr<Material> Material)
+                 shared_ptr<Material> MaterialPtr)
                  : CenterStart(CenterStart), CenterEnd(CenterEnd),
                    MovementStartTime(StartMovingAt), MovementEndTime(StopMovingAt),
                    Radius(Radius),
-                   Material(Material)
+                   MaterialPtr(MaterialPtr)
     {
         // Do nothing
     };
@@ -34,11 +34,58 @@ private:
     Point3 CenterStart, CenterEnd;
     double MovementStartTime, MovementEndTime;
     double Radius;
-    shared_ptr<Material> Material;
+    shared_ptr<Material> MaterialPtr;
 };
 
 Point3 MovingSphere::CenterPositionAt(double Time) const {
     return CenterStart + ((Time - MovementStartTime) / (MovementEndTime - MovementStartTime)) * (CenterEnd - CenterStart);
+}
+
+bool MovingSphere::Hit(const Ray &Ray, double DepthMin, double DepthMax, HitRecord &Record) const {
+    Vector3 oc = Ray.getRayOrigin() - CenterPositionAt(Ray.getCreatedTime());
+    auto a = Ray.getRayDirection().lengthSquared();
+    auto half_b = dotProduct(oc, Ray.getRayDirection());
+    auto c = oc.lengthSquared() - Radius * Radius;
+
+    auto discriminant = half_b * half_b - a*c;
+
+    // Sphere is Hit by the Ray if and only if the equation has real solutions
+    if (discriminant > 0)
+    {
+        // Solve for the solution that contains the actual parameter to get the point.
+        auto root = sqrt(discriminant);
+
+        // try smaller 'Depth' first
+        auto temp = (-half_b - root) / a;
+        if (temp < DepthMax && temp > DepthMin)
+        {
+            Record.Depth = temp;
+            // the point of the surface that was Hit by the Ray
+            Record.HitPoint = Ray.getPointAt(Record.Depth);
+            // here, we define a HitPointNormal vector to point outward
+            Vector3 OutwardNormal = (Record.HitPoint - CenterPositionAt(Ray.getCreatedTime())) / Radius;
+            // compare the getRayDirection of the Ray & OutwardNormal
+            // set the HitPointNormal, opposite to the getRayDirection where light came from
+            Record.setFaceNormal(Ray, OutwardNormal);
+            Record.MaterialPtr = MaterialPtr;
+            return true;
+        }
+
+        // try larger 'Depth' then
+        temp = (-half_b + root) / a;
+
+        if (temp < DepthMax && temp > DepthMin)
+        {
+            Record.Depth = temp;
+            Record.HitPoint = Ray.getPointAt(Record.Depth);
+            Vector3 OutwardNormal = (Record.HitPoint - CenterPositionAt(Ray.getCreatedTime())) / Radius;
+            Record.setFaceNormal(Ray, OutwardNormal);
+            Record.MaterialPtr = MaterialPtr;
+            return true;
+        }
+    }
+
+    return false;
 }
 
 #endif //RTFOUNDATION_MOVINGSPHERE_HPP
