c60d728afb85c32588daa33e7be9ce531e75eaa1
Yoo SeungWoo <dreamy1534@gmail.com>
2021-12-20 20:06:56 +0900

    Add scene 'Final Scene'

--- rt-the-next-week/src/cpu/main.cpp
+++ rt-the-next-week/src/cpu/main.cpp
@@ -234,6 +234,70 @@ HittableList generateCornellSmoke() {
     return objects;
 }
 
+HittableList generateFinalScene() {
+    HittableList boxes1;
+    auto ground = make_shared<Lambertian>(Color(0.48, 0.83, 0.53));
+
+    const int boxes_per_side = 20;
+    for (int i = 0; i < boxes_per_side; i++) {
+        for (int j = 0; j < boxes_per_side; j++) {
+            auto w = 100.0;
+            auto x0 = -1000.0 + i * w;
+            auto z0 = -1000.0 + j * w;
+            auto y0 = 0.0;
+            auto x1 = x0 + w;
+            auto y1 = generateRandomDouble(1, 101);
+            auto z1 = z0 + w;
+
+            boxes1.add(make_shared<Box>(Point3(x0, y0, z0), Point3(x1, y1, z1), ground));
+        }
+    }
+
+    HittableList objects;
+
+    objects.add(make_shared<BVHNode>(boxes1, 0, 1));
+
+    auto light = make_shared<DiffuseLight>(Color(7, 7, 7));
+    objects.add(make_shared<XZRectangle>(123, 423, 147, 412, 554, light));
+
+    auto center1 = Point3(400, 400, 200);
+    auto center2 = center1 + Vector3(30, 0, 0);
+    auto moving_sphere_material = make_shared<Lambertian>(Color(0.7, 0.3, 0.1));
+    objects.add(make_shared<MovingSphere>(center1, center2, 0, 1, 50, moving_sphere_material));
+
+    objects.add(make_shared<Sphere>(Point3(260, 150, 45), 50, make_shared<Dielectric>(1.5)));
+    objects.add(make_shared<Sphere>(
+            Point3(0, 150, 145), 50, make_shared<Metal>(Color(0.8, 0.8, 0.9), 1.0)
+    ));
+
+    auto boundary = make_shared<Sphere>(Point3(360,150,145), 70, make_shared<Dielectric>(1.5));
+    objects.add(boundary);
+    objects.add(make_shared<ConstantMedium>(boundary, 0.2, Color(0.2, 0.4, 0.9)));
+    boundary = make_shared<Sphere>(Point3(0, 0, 0), 5000, make_shared<Dielectric>(1.5));
+    objects.add(make_shared<ConstantMedium>(boundary, .0001, Color(1,1,1)));
+
+    auto emat = make_shared<Lambertian>(make_shared<ImageTexture>("../../../../data/earthmap.jpeg"));
+    objects.add(make_shared<Sphere>(Point3(400,200,400), 100, emat));
+    // auto pertext = make_shared<noise_texture>(0.1);
+    // objects.add(make_shared<sphere>(point3(220,280,300), 80, make_shared<lambertian>(pertext)));
+
+    HittableList boxes2;
+    auto white = make_shared<Lambertian>(Color(.73, .73, .73));
+    int ns = 1000;
+    for (int j = 0; j < ns; j++) {
+        boxes2.add(make_shared<Sphere>(Point3::random(0,165), 10, white));
+    }
+
+    objects.add(make_shared<TranslateInstance>(
+                        make_shared<YRotationInstance>(
+                                make_shared<BVHNode>(boxes2, 0.0, 1.0), 15),
+                        Vector3(-100,270,395)
+                )
+    );
+
+    return objects;
+}
+
 int main() {
     // configure OpenMP
     omp_set_num_threads(8);
@@ -259,7 +323,7 @@ int main() {
     // set world
     BVHNode WorldBVH;
     HittableList World;
-    int SceneSelector = 4;
+    int SceneSelector = 8;
 
     switch (SceneSelector) {
         case 0:
@@ -323,6 +387,17 @@ int main() {
             ImageWidth = 600;
             SamplesPerPixel = 200;
             LookFrom = Point3(278, 278, -800);
+            LookAt = Point3(278, 278, 0);
+            VerticalFOV = 40.0;
+            break;
+
+        case 8:
+            World = generateFinalScene();
+            AspectRatio = 1.0;
+            ImageWidth = 800;
+            SamplesPerPixel = 10000;
+            BackgroundColor = Color(0,0,0);
+            LookFrom = Point3(478, 278, -600);
             LookAt = Point3(278, 278, 0);
             VerticalFOV = 40.0;
             break;
